8b6b90ec83614f32e9fb5ec04d2236762943803d
anddieguez <andreadiegar@gmail.com>
2023-11-15 08:15:52 +0100

    added db

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//imports
+
+const express = require("express");
+const cors = require("cors");
+const mysql = require("mysql2/promise");
+
+//arrancar el servidor
+const app = express();
+
+//configurar
+
+app.use(cors());
+app.use(express.json());
